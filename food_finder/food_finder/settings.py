241a2f64041323364fa3b1a8a1d61dd1871d9026
Atsushi <todaatsushi0@gmail.com>
2019-04-04 16:52:10 +0100

    Base project skeleton done

--- food_finder/food_finder/settings.py
+++ food_finder/food_finder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import secret as s
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vx%46(digf_+gil24f-*=9^)telxwlh=(x(hvxn0m257^jn#2q'
+SECRET_KEY = s.RF_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
