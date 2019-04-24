d461b7f40f4c5ba4f2877ebb5bc5143f4c375988
Atsushi <todaatsushi0@gmail.com>
2019-04-24 19:32:37 +0100

    settings path in wsgi

--- food_finder/food_finder/wsgi.py
+++ food_finder/food_finder/wsgi.py
@@ -3,6 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_finder.food_finder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_finder.settings')
 
 application = get_wsgi_application()
